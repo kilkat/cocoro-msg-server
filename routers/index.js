7e8647a9c3ef5d48e7ba3d844257d458ece38ab0
kilkat <rhkddns3459@naver.com>
2024-06-25 01:26:09 +0900

    (24-0625)ctrl_분리_완료

--- routers/index.js
+++ routers/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers');
+const user_ctrl = require('../controllers/user.ctrl');
+const chat_ctrl = require('../controllers/chat.ctrl');
 const authenticateToken = require('../middlewares/auth');
 
-router.get('/', controller.getTest);
-
 //user 관련
-router.post('/user/userCreate', controller.userCreate);
-router.post('/user/userLogin', controller.userLogin);
-router.post('/user/userSearch', controller.userSearch);
-router.post('/user/addFriend', controller.addFriend);
+router.post('/user/userCreate', user_ctrl.userCreate);
+router.post('/user/userLogin', user_ctrl.userLogin);
+router.post('/user/userSearch', user_ctrl.userSearch);
+router.post('/user/addFriend', user_ctrl.addFriend);
 
 //chat 관련
-router.post('/chat/chatCreate', controller.chatCreate);
+router.post('/chat/chatCreate', chat_ctrl.chatCreate);
 
 module.exports = router;
